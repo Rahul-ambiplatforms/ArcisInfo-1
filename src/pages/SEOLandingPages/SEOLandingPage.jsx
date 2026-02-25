db3a6a450e6d1d76a61bffc6bc214cb95976a444
vmukti <81781432+vmukti@users.noreply.github.com>
2026-02-25 11:28:09 +0530

    feat: wire seoPageDataExpansion3 into allSeoData routing

--- src/pages/SEOLandingPages/SEOLandingPage.jsx
+++ src/pages/SEOLandingPages/SEOLandingPage.jsx
@@ -9,13 +9,14 @@ import seoPageDataCompare from "../../data/seoPageDataCompare";
 import seoPageDataCompliance from "../../data/seoPageDataCompliance";
 import seoPageDataExpansion2 from "../../data/seoPageDataExpansion2";
 import seoPageDataGeoIntl from "../../data/seoPageDataGeoIntl";
+import seoPageDataExpansion3 from "../../data/seoPageDataExpansion3";
 
 const SEOLandingPage = () => {
   const params = useParams();
   const { category, pageSlug, city, slug, seriesId } = params;
 
   // Multi-strategy key lookup
-  const allSeoData = { ...seoPageData, ...seoPageDataExpansion, ...seoPageDataGeo, ...seoPageDataCompare, ...seoPageDataCompliance, ...seoPageDataExpansion2, ...seoPageDataGeoIntl };
+  const allSeoData = { ...seoPageData, ...seoPageDataExpansion, ...seoPageDataGeo, ...seoPageDataCompare, ...seoPageDataCompliance, ...seoPageDataExpansion2, ...seoPageDataGeoIntl, ...seoPageDataExpansion3 };
   const lookupKey = (city && allSeoData[`cctv-cameras-${city}`]) ? `cctv-cameras-${city}`
     : (category && pageSlug && allSeoData[`${category}-${pageSlug}`]) ? `${category}-${pageSlug}`
     : (pageSlug && allSeoData[pageSlug]) ? pageSlug
