20f35c7b76a5956560cd7428209bf7af147cc125
vmukti-dev-agent <kushal@vmukti.com>
2026-06-22 11:27:16 +0530

    feat: India CCTV market & certification report 2026 (src/Components/Footer/Footer.js)

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -20,7 +20,6 @@ import FacebookIcon from "../Icons/facebook_icon.svg";
 import InstagramIcon from "../Icons/instagram_icon.svg";
 import LinkedInIcon from "../Icons/linkedIn_icon.svg";
 import XIcon from "../Icons/X_icon.svg";
-import YoutubeIcon from "../Icons/youtube_icon.svg";
 import NextLink from "next/link";
 
 const ListHeader = ({ children }) => {
@@ -82,7 +81,6 @@ const Footer = () => {
     twitter: "https://x.com/arcisai",
     instagram: "https://www.instagram.com/_arcisai_/",
     linkedin: "https://www.linkedin.com/company/thearcisai/",
-    youtube: "https://www.youtube.com/@arcisai",
   };
 
   return (
@@ -228,6 +226,9 @@ const Footer = () => {
               <FooterLink href={"/cctv-compliance-2026"}>
                 CCTV Compliance 2026 Guide
               </FooterLink>
+              <FooterLink href={"/india-cctv-market-report-2026"}>
+                CCTV Market Report 2026
+              </FooterLink>
             </Stack>
 
             {/* TOP LOCATIONS — internal links so city pages aren't orphaned */}
@@ -268,7 +269,6 @@ const Footer = () => {
               <SocialButton icon={XIcon} href={socialLinks.twitter} />
               <SocialButton icon={InstagramIcon} href={socialLinks.instagram} />
               <SocialButton icon={LinkedInIcon} href={socialLinks.linkedin} />
-              <SocialButton icon={YoutubeIcon} href={socialLinks.youtube} />
             </HStack>
           </Stack>
         </Flex>
